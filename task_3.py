e4457de38ae7a4c064ed2d4f115adb46b7750b45
pulseofmalstrem <lispandhaskell@gmail.com>
2016-09-27 09:29:36 +0300

    made takst 1-6

--- task_3.py
+++ task_3.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 
 from pyrob.api import *
 
 
 @task
 def task_3_1():
-    pass
+    while wall_is_on_the_right() == False :
+        move_right(1)
+
 
 
 if __name__ == '__main__':
