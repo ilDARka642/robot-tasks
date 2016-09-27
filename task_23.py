15f29d647d333b07ad89730141512d95540702ae
pulseofmalstrem <lispandhaskell@gmail.com>
2016-09-27 12:12:19 +0300

    made task 1-23

--- task_23.py
+++ task_23.py
@@ -5,8 +5,26 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_6_6():
-    pass
-
+    move_right()
+    p=0
+    while wall_is_on_the_right()==False:
+        p+=1
+        if wall_is_above()==False:
+            k=0
+            while wall_is_above()==False:
+                move_up()
+                fill_cell()
+                k+=1
+            move_down(k)
+        move_right()
+    if wall_is_on_the_right()==True and wall_is_above()==False:
+        k=0
+        while wall_is_above()==False:
+            move_up()
+            fill_cell()
+            k+=1
+        move_down(k)
+    move_left(p+1)
 
 if __name__ == '__main__':
     run_tasks()
